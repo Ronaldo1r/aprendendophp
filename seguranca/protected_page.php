6f97027ffbfd0df24432c3d685d922119e519364
Ronaldo1r <Ronaldo.c.faria@hotmail.com>
2024-06-21 08:00:32 -0300

    finalizado

--- seguranca/protected_page.php
+++ seguranca/protected_page.php
@@ -6,22 +6,22 @@ sec_session_start();
 ?>
 <!DOCTYPE html>
 <html>    
-    <head>        
-        <meta charset="UTF-8">        
-        <title>Secure Login: Protected Page</title>        
-        <link rel="stylesheet" href="styles/main.css" />    
+<head>        
+    <meta charset="UTF-8">        
+    <title>Secure Login: Protected Page</title>        
+    <link rel="stylesheet" href="styles/main.css" />    
     </head>    
     <body>
-        <?php if (login_check($mysqli) == true) : ?>
-            <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>    
-            <p>
-            Esta é uma pagina protegida para servir de exemplo. Para acessá-la, os usuários devem ter feito o login. Em dado momento, também verificaremos o papel que o usuário está desempenhando para que possamos determinar o tipo de usuário que está autorizado a acessar a página. 
-            </p>            
-            <p>Return to <a href="index.php">login page</a></p>
-            <?php else : ?>            
-                <p>                
-                    <span class="error">Você não tem autorização para acessar esta página.</span>  Please <a href="index.php">login</a>.
-                        </p>
-                        <?php endif; ?>    
-                    </body>
-                </html>
+<?php if (login_check($mysqli) == true) : ?>
+    <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>    
+    <p>
+    Esta é uma pagina protegida para servir de exemplo. Para acessá-la, os usuários devem ter feito o login. Em dado momento, também verificaremos o papel que o usuário está desempenhando para que possamos determinar o tipo de usuário que está autorizado a acessar a página. 
+    </p>            
+    <p>Return to <a href="index.php">login page</a></p>
+    <?php else : ?>            
+    <p>                
+    <span class="error">Você não tem autorização para acessar esta página.</span>  Please <a href="index.php">login</a>.
+    </p>
+    <?php endif; ?>    
+    </body>
+</html>
